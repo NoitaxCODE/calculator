29570cf03b53fcee29569db3bea327cb5ba6608e
Gaston Pappaterra <gpappaterra@hotmail.com>
2024-01-25 02:16:01 -0300

    creo displayFunctions.js y le mando las funciones de display y configuro btn de sonido

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-import { addNumber, resultNumber, subtractNumber, setCounter, setAccumulated, setOperation, multiplyNumber, divideNumber, percentNumber } from "./helpers/operations.js";
+import { addComa, clearDisplay, clearDisplayText, disableEffect, enableDisableAudio, showDisplay, styleButton } from "./helpers/displayFunctions.js";
+import { addNumber, resultNumber, subtractNumber, multiplyNumber, divideNumber, percentNumber } from "./helpers/operations.js";
 
 const numbers = document.querySelectorAll('.number');
 const items = document.querySelectorAll('.item');
-const displayText = document.querySelector('#displayText');
 const clear = document.querySelector('#clear');
 const coma = document.querySelector('#coma');
 const add = document.querySelector('#add');
@@ -68,11 +68,11 @@ numbers.forEach(number => {
 });
 
 items.forEach(item => {
-  item.addEventListener('click', (e)=>{
-    styleButton(e);
+  item.addEventListener('click', ()=>{
+    styleButton.bind( this, items );
     audio.play();
   })
-})
+});
 
 clear.addEventListener('click', ()=>{
   clearDisplay();
@@ -107,102 +107,11 @@ percent.addEventListener('click', ()=> {
 });
 
 sound.addEventListener('click', ()=> {
-  enableDisableAudio();
+  enableDisableAudio( audio );
+  disableEffect( sound );
 });
 
 
-
-const styleButton = ({ target })=> {
-
-  if ( target.tagName === 'DIV' ) {
-    target.classList.toggle("pressButton");
-  }else{
-    target.parentElement.classList.toggle("pressButton");
-  };
-
-  setTimeout( ()=> { 
-    items.forEach( item => {
-      item.classList.remove("pressButton")
-    }
-  )}, 150)
-
-};
-
-const formatDisplayNumber = ( displayText, content )=> {
-
-  // PASO EL STRING A ARRAY
-  const displayTextArray = displayText.split('');
-
-  // QUITO LOS PUNTOS
-  let arrayClear = displayTextArray.filter((word) => word !== '.');
-
-  // SI HAY COMAS LAS CAMBIO POR PUNTO PARA PODER FORMATEARLO A ESPAÑOL
-  if ( arrayClear.indexOf(',') > -1 ) arrayClear.splice(arrayClear.indexOf(','), 1, '.')
-
-  //DETECTO SI HAY UN .0
-  let decimal0 = false
-  if ( ( arrayClear.join('') + content ).indexOf('.0') > -1 ) decimal0 = true
-
-
-  // CONVIERTO EL ARRAY EN STRING, LE CONCATENO EL VALOR NUEVO, LO CONVIERTO A NUMERO Y LE DOY FORMATO DE NUMERO ESPAÑOL
-  // CREO EL IF PARA RESOLVER EL PROBLEMA CON EL .0
-  
-  let result
-  if( !decimal0 ){
-    result = ( parseFloat( arrayClear.join('') + content )).toLocaleString('es-ES', { maximumFractionDigits: 7 })
-  } else{
-    result = arrayClear.join('').replace('.',',') + content 
-  }
-
-
-
-  return result
-
-}
-
-const addComa = ()=> {
-
-  //SI HAY MAS DE UNA COMA TIRO ERROR
-  if ( displayText.textContent.indexOf(',') > -1 ) { 
-    displayText.innerHTML = `<p class="displayText">ERROR</p>` 
-    return
-  }
-
-  displayText.innerHTML = `<p class="displayText">${ displayText.textContent + ',' }</p>`
-  
-
-}
-
-const showDisplay = ( e )=>{
-
-  const content = e.target.innerText
-  displayText.textContent = formatDisplayNumber( displayText.textContent, content ) 
-
-}
-
-const clearDisplay = ()=>{
-
-  const display2 = document.querySelector('#display2');
-  displayText.textContent = '0';
-  if( display2 ) display2.remove();
-  setAccumulated(0);
-  setCounter(0);
-  setOperation('');
-}
-
-const clearDisplayText = ()=> {
-
-  // EL COUNTER LO USO PARA LIMPIAR EL DISPLAY LUEGO QUE COMIENZO UNA OPERACION
-  if ( setCounter() >= 1 ) { 
-    displayText.textContent = '0';
-    setCounter(0);
-  }
-
-}
-
-const enableDisableAudio = ()=>{
-  ( audio.volume === 0 ) ? audio.volume = 1 : audio.volume = 0;
-}
 
 
 
