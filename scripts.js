70a0c6cdadedbc92292e3ff58d63506eb23bfb9b
Gaston Pappaterra <gpappaterra@hotmail.com>
2024-02-03 17:46:44 -0300

    configuro la app para que sea una pwa

--- scripts.js
+++ scripts.js
@@ -1,126 +1,153 @@
-import { addComa, clearDisplay, clearDisplayText, disableEffect, enableDisableAudio, showDisplay, showDisplay2Bg, styleButton } from "./helpers/displayFunctions.js";
+import {
+  addComa,
+  clearDisplay,
+  clearDisplayText,
+  disableEffect,
+  enableDisableAudio,
+  showDisplay,
+  showDisplay2Bg,
+  styleButton,
+} from "./helpers/displayFunctions.js";
 import { getAudioUrl } from "./helpers/getAudio.js";
-import { addNumber, resultNumber, subtractNumber, multiplyNumber, divideNumber, percentNumber, backButton } from "./helpers/operations.js";
+import {
+  addNumber,
+  resultNumber,
+  subtractNumber,
+  multiplyNumber,
+  divideNumber,
+  percentNumber,
+  backButton,
+} from "./helpers/operations.js";
 
-const numbers = document.querySelectorAll('.number');
-const items = document.querySelectorAll('.item');
-const clear = document.querySelector('#clear');
-const coma = document.querySelector('#coma');
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const result = document.querySelector('#result');
-const multiply = document.querySelector('#multiply');
-const divide = document.querySelector('#divide');
-const percent = document.querySelector('#percent');
-const sound = document.querySelector('#sound');
-const back = document.querySelector('#back');
-const audio = new Audio( await getAudioUrl() );
+const numbers = document.querySelectorAll(".number");
+const items = document.querySelectorAll(".item");
+const clear = document.querySelector("#clear");
+const coma = document.querySelector("#coma");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const result = document.querySelector("#result");
+const multiply = document.querySelector("#multiply");
+const divide = document.querySelector("#divide");
+const percent = document.querySelector("#percent");
+const sound = document.querySelector("#sound");
+const back = document.querySelector("#back");
+const audio = new Audio(await getAudioUrl());
 
-document.addEventListener('keydown', ({ key })=> {
-  
-  if ( key === 'Enter' ) { 
-    result.click()
-    return
-  };
-  if ( key === '+' ) {
-    add.click()
-    return
-  };
-  if ( key === '-' ) {
-    subtract.click() 
-    return
-  };
-  if ( key === '.' ) {
-    coma.click() 
-    return
-  };
-  if ( key === 'Delete' ) {
-    clear.click() 
-    return
-  };
+document.addEventListener("keydown", ({ key }) => {
+  if (key === "Enter") {
+    result.click();
+    return;
+  }
+  if (key === "+") {
+    add.click();
+    return;
+  }
+  if (key === "-") {
+    subtract.click();
+    return;
+  }
+  if (key === ".") {
+    coma.click();
+    return;
+  }
+  if (key === "Delete") {
+    clear.click();
+    return;
+  }
 
-  if ( key === '*' ) {
-    multiply.click() 
-    return
-  };
+  if (key === "*") {
+    multiply.click();
+    return;
+  }
 
-  if ( key === '/' ) {
-    divide.click() 
-    return
-  };
+  if (key === "/") {
+    divide.click();
+    return;
+  }
 
-  if ( key === '%' ) {
-    percent.click() 
-    return
-  };
-  if ( key === 'Backspace' ) {
-    backButton() 
-    return
-  };
+  if (key === "%") {
+    percent.click();
+    return;
+  }
+  if (key === "Backspace") {
+    backButton();
+    return;
+  }
 
-  if ( isNaN( key ) ) return;
+  if (isNaN(key)) return;
 
-  numbers.forEach(number => {
-    if ( number.textContent === key ) number.click();
+  numbers.forEach((number) => {
+    if (number.textContent === key) number.click();
   });
-
 });
 
-numbers.forEach(number => {
-  number.addEventListener('click', (e)=>{
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
     clearDisplayText();
     showDisplay(e);
   });
 });
 
-items.forEach(item => {
-  item.addEventListener('click', styleButton.bind(this, items) );
-  item.addEventListener('click', ()=> { audio.play()  });
+items.forEach((item) => {
+  item.addEventListener("click", styleButton.bind(this, items));
+  item.addEventListener("click", () => {
+    audio.play();
+  });
 });
 
-clear.addEventListener('click', ()=>{
+clear.addEventListener("click", () => {
   clearDisplay();
-
 });
 
-coma.addEventListener('click', ()=>{
+coma.addEventListener("click", () => {
   addComa();
 });
 
-add.addEventListener('click', ()=>{
+add.addEventListener("click", () => {
   showDisplay2Bg();
   addNumber();
 });
 
-result.addEventListener('click', ()=>{
+result.addEventListener("click", () => {
   resultNumber();
 });
 
-subtract.addEventListener('click', ()=>{
+subtract.addEventListener("click", () => {
   showDisplay2Bg();
   subtractNumber();
 });
 
-multiply.addEventListener('click', ()=>{
+multiply.addEventListener("click", () => {
   showDisplay2Bg();
   multiplyNumber();
 });
 
-divide.addEventListener('click', ()=> {
+divide.addEventListener("click", () => {
   showDisplay2Bg();
   divideNumber();
 });
 
-percent.addEventListener('click', ()=> {
+percent.addEventListener("click", () => {
   percentNumber();
 });
 
-sound.addEventListener('click', ()=> {
-  enableDisableAudio( audio );
-  disableEffect( sound );
+sound.addEventListener("click", () => {
+  enableDisableAudio(audio);
+  disableEffect(sound);
 });
 
-back.addEventListener('click', ()=>{
+back.addEventListener("click", () => {
   backButton();
 });
 
+if ("serviceWorker" in navigator) {
+  (async () => {
+    try {
+      let { active } = await navigator.serviceWorker.register("./sw.js");
+
+      active.state === "activated" && console.log("Registro de SW exitoso");
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+}
---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "v1_cache_calculator";
+const urlsToCache = [
+  "./",
+  "./style.css",
+  "./scripts.js",
+  "./media/favicon-16x16.png",
+  "./media/sound.svg",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("Fallo registro de cache", err)),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWithelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        cacheNames.map((cacheName) => {
+          if (cacheWithelist.indexOf(cacheName) === -1)
+            return caches.delete(cacheName);
+        });
+      })
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) return res;
+      return fetch(e.request);
+    }),
+  );
+});
