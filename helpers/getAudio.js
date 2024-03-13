3cf30cd583633556267d69de6cd6f24c3d473512
Gaston Pappaterra <gpappaterra@hotmail.com>
2024-03-13 19:45:05 -0300

    Arreglo un par de cosas antes de subir la app a la playstore

--- helpers/getAudio.js
+++ helpers/getAudio.js
@@ -7,7 +7,6 @@ export const getAudioUrl = async () => {
 
     const resp = await fetch('https://audius-discovery-16.cultur3stake.com/v1/tracks/jYAq5pM/stream?app_name=CALCULATOR', {
       method: 'GET',
-
       headers: headers
     })
 
